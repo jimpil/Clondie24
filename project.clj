e5918d88b5e6c6f54b77d7317dbc2081895ae42b
Dimitrios Piliouras <jimpil1985@gmail.com>
2012-06-07 15:10:48 +0100

    first projetc spec

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject Clondie24 "0.1.0-SNAPSHOT"
+  :description "Blondie24 Extreme-Makeover! "
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 ;[clojure-encog "0.4.0-SNAPSHOT"]
+                 ]
+                 
+  :jvm-opts ["-Xmx1g"] 
+ ;:javac-options {:classpath "target/dependency/encog-core-3.1.0.jar" :destdir "target/classes"}
+ ;:java-source-path "src/java"
+ ;:main     Clondie24.core             
+ )
