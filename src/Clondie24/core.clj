029ea3d82d69514ff415fa0c3cf97561db7322a7
Dimitrios Piliouras <jimpil1985@gmail.com>
2012-06-07 22:43:23 +0100

    added board mappings from 1d to 2d and vice versa

--- src/Clondie24/core.clj
+++ src/Clondie24/core.clj
@@ -3,12 +3,39 @@
 
 )
 
+
+(def ^:const board-mappings-checkers 
+"A vector of vectors. Outer vector represents the 32 (serial) positions checkers can position themselves on. 
+Each inner vector represents the coordinates of that position on the 8x8 grid."
+[ ;;
+[1 0] [3 0] [5 0] [7 0] 
+[6 1] [4 1] [2 1] [0 1]
+[1 2] [3 2] [5 2] [7 2] 
+[6 3] [4 3] [2 3] [0 3]
+[1 4] [3 4] [5 4] [7 4]
+[6 5] [4 5] [2 5] [0 5]
+[1 6] [3 6] [5 6] [7 6]
+[6 7] [4 7] [2 7] [0 7]
+])
+
+(def :^const board-mappings-chess nil) ;TODO
+
+(def ^:dynamic black-direction -1)
+(def ^:dynamic white-direction 1)
+
 (def valid-checkers-positions (range 32))
 (def invalid-checkers-positions (take 32 (repeat -1)))
 
 (def checkers-1d ;the checkers-board as a list. -1 represents invalid positions
 (interleave invalid-checkers-positions 
             valid-checkers-positions))
+            
+#_(def starting-board [game] 
+(if (= game :checkers) 
+(zipmap (range 32) 
+(cons (take 12 (repeat 
+      (make-piece false java.awt.Color/Yellow (make-point 1 5) :rank 'soldier))) '() ))
+))
 
 (def chess-1d (range 64)) ;;the chess board as a list
 
@@ -21,6 +48,15 @@
 (def chess-ranks '(:queen :rook :knight :bishop :pawn :king)) ;all the different chess piece-types 
 (def rel-values  '(9 5 3 3 1 500))  ;relative values according to wikipedia
 (def chess-rel-values (zipmap chess-ranks rel-values)) ;=> {:king 500, :pawn 1, :bishop 3, :knight 3, :rook 5, :queen 9}
+
+(defn translate-position
+"Translates a position from 1d to 2d and vice-versa. 
+Mappings should be either 'checkers-board-mappings' or 'chess-board-mappings'." 
+([i mappings]   ;will translate from 1d to 2d
+(get mappings i)) 
+([x y mappings] ;will translate from 2d to 1d
+(.indexOf mappings (vector x y))
+)) 
 
  
 (declare make-piece) ;will need this fn in here (when promoting)
@@ -37,7 +73,7 @@
                            rank] 
  Piece 
  (update-position [this np] ;mutable state inside Point!
-   (.setLocation position ;can accept ints or doubles
+   (. position setLocation  ;can accept ints or doubles
      (first np) (second np))) ;np should be [x, y]
  (die [this]     (vary-meta this assoc :dead true)) ;communicate death through meta-data 
  (promote [this] (make-piece false color position :rank 'prince)) ; a checker is promoted to prince
@@ -51,7 +87,7 @@
                         rank]
  Piece 
  (update-position [this np] ;mutable state inside Point!
-   (.setLocation position ;can accept ints or doubles
+   (. position setLocation  ;can accept ints or doubles
      (first np) (second np))) ;np should be [x, y]
  (die [this]     (vary-meta this assoc :dead true)) ;communicate death through meta-data 
  (promote [this] (make-piece true image position :rank 'queen)) ;a pawn is promoted to a queen
