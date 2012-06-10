f9163ba58b63e1a71b59cdc57ca9c387f9360dde
Dimitrios Piliouras <jimpil1985@gmail.com>
2012-06-10 14:22:50 +0100

    some initial thoughts about the rules

--- src/Clondie24/rules.clj
+++ src/Clondie24/rules.clj
@@ -1,23 +1,22 @@
 (ns Clondie24.rules)
 
-(defn ^boolean diagonally? 
+(defn ^Boolean diagonally? 
 "Returns true if moving diagonally, false otherwise. Expects 2 vectors denoting start/end poisitions." 
 [from to] )
-(defn ^boolean forward? 
+(defn ^Boolean forward? 
 "Returns true if moving forwards, false otherwise. Expects 2 vectors denoting start/end poisitions." 
 [from to])
-(defn ^boolean backward? 
+(defn ^Boolean backward? 
 "Returns true if moving backwards, false otherwise. Expects 2 vectors denoting start/end positions."
 [from to])
-(defn ^boolean left? 
+(defn ^Boolean left? 
 "Returns true if moving left, false otherwise. Expects 2 vectors denoting start/end positions."
 [from to])
-(defn ^boolean right? 
+(defn ^Boolean right? 
 "Returns true if moving right , false otherwise. Expects 2 vectors denoting start/end positions." 
 [from to])
 
-(defn ^boolean legal-move? 
+(defn ^Boolean legal-move? 
 "Returns true if the move is legal according to the rules , false otherwise. Expects a direction (1/-1) and 2 vectors denoting start/end positions ." 
 [board direction from to])
 
-(defn vacant? [index])
