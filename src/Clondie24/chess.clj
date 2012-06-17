d48aa601782811f3dc065500b3e6a64f088c3152
Dimitrios Piliouras <jimpil1985@gmail.com>
2012-06-17 15:12:25 +0100

    chess-game sorted...rules come next

--- src/Clondie24/chess.clj
+++ src/Clondie24/chess.clj
@@ -1,6 +1,7 @@
 (ns Clondie24.chess 
     (:require [Clondie24.util :as ut] 
-              [Clondie24.core :as core])
+              [Clondie24.core :as core]
+              [Clondie24.rules :as rul])
 )
 
 ;----------------------------------------<SOURCE>--------------------------------------------------------------------
@@ -21,33 +22,59 @@
 ])
 
 
-
-(def chessPos->rank 
-(flatten ['rook 'knight 'bishop 'queen 'king 'bishop 'knight 'rook (repeat 8 'pawn)]))
-
-(defn starting-chessItems
-"Will construct a set of initial checkers (12). opponent? specifies the side of the board where the pieces should be placed (true for north false for south)."
-[opponent?]
-(if opponent?  
-(map #(make-chessItem (second (chess-images (keyword %2))) 
-      (translate-position % (get (details) :mappings)) :rank %2) (range 16) chessPos->rank)
-(map #(make-chessItem (first (chess-images (keyword %2))) 
-      (translate-position % (get (details) :mappings)) :rank %2) (range 48 64) (reverse chessPos->rank))      
-))
-
 (def chess-images 
-(zipmap (get (details) :ranks)[
+(zipmap '(:queen :rook :knight :bishop :pawn :king)
+                     [
                       [(ut/make-image "white queen-image")
                        (ut/make-image "black queen-image")]
                       [(ut/make-image "white rook-image")
                        (ut/make-image "black rook-image")]
                       [(ut/make-image "white knight-image")
                        (ut/make-image "black knight-image")]
+                      [(ut/make-image "white bishop-image")
+                       (ut/make-image "black bishop-image")]
                       [(ut/make-image "white pawn-image")
                        (ut/make-image "black pawn-image")]
                       [(ut/make-image "white king-image")
                        (ut/make-image "black king-image")]
                       ]))
+
+(def chessPos->rank 
+(flatten ['rook 'knight 'bishop 'queen 'king 'bishop 'knight 'rook (repeat 8 'pawn)]))
+
+(declare make-chessItem, details) ;;will need these
+
+(defn starting-chessItems
+"Will construct a set of initial checkers (12). opponent? specifies the side of the board where the pieces should be placed (true for north false for south)."
+[opponent?]
+(if opponent?  
+(map #(make-chessItem (second (chess-images (keyword %2))) 
+      (core/translate-position % board-mappings-chess) :rank %2) (range 16) chessPos->rank)
+(map #(make-chessItem (first (chess-images (keyword %2))) 
+      (core/translate-position % board-mappings-chess) :rank %2) (range 48 64) (reverse chessPos->rank))      
+))
+
+(def chess-moves {:pawn     nil
+                  :rook     nil
+                  :bishop   nil
+                  :knight   #(rul/knight-moves %1 %2)
+                  :queen    nil 
+                  :king     nil})
+                  
+(defn rank->moves 
+"Returns all the legal moves of p depending on rank of p." 
+[p] 
+(let [gpos (core/getGridPosition p)]
+((get chess-moves (keyword (core/getRank p))) ;will return a fn
+  (first gpos) (second gpos))))
+
+                      
+(def current-chessItems
+"This is list that keeps track of moving checkers. Is governed by an atom and it changes after every move. All changes are being logged to 'board-history'. Starts off as nil but we can always get the initial arrangement from core."
+(add-watch 
+(atom nil #_(vec (core/starting-board (details))) ) 
+      ;:validator #(== 32 (count %))   
+  :log (partial core/log-board core/board-history)))                       
                       
 (defn details "Returns a map that describes the game of chess."
 ^clojure.lang.PersistentArrayMap []
@@ -61,52 +88,47 @@
                :rel-values (zipmap '(:queen :rook :knight :bishop :pawn :king) 
                                    '(  9      5     3       3       1     100))
                :board-atom current-chessItems
-               :record-name "ChessPiece" 
+               :record-name "Clondie24.chess.ChessPiece" 
                :mappings board-mappings-chess
                :north-player-start  (starting-chessItems true)
                :south-player-start  (starting-chessItems false)})
                
-(def current-chessItems
-"This is list that keeps track of moving checkers. Is governed by an atom and it changes after every move. All changes are being logged to 'board-history'."
-(add-watch 
-(atom (vec (core/starting-board (details))) ) 
-      ;:validator #(== 32 (count %))   
-  :log (partial core/log-board core/board-history))) 
   
 ;partially apply move with game and chess-mappings locked in as 1st & 2nd args
 (def move-chessItem (partial core/move  (details)))  
 (def make-chessItem (partial core/make-piece (details))) 
-(def vacant-chess-tile? (partial ut/vacant? (get (details) mappings)))
+(def vacant-chess-tile? (partial core/vacant? (get (details) :mappings)))
 
 (defrecord ChessPiece [^java.awt.Image image 
                        ^clojure.lang.PersistentVector position 
                         rank ^Integer value]
- Piece 
+ core/Piece 
  (update-position [this np] (make-chessItem image position rank))
  (die [this]     (vary-meta this assoc :dead true)) ;communicate death through meta-data 
  (promote [this] (make-chessItem image position :rank 'queen)) ;a pawn is promoted to a queen
  (getGridPosition [this] position)
  (getListPosition [this] (core/translate-position (first  position) 
-                                                  (second position) (get (chess) :mappings)))
- (getPoint [this] (make-point position))
- (getMoves [this] nil) ;TODO 
+                                                  (second position) (get (details) :mappings)))
+ (getPoint [this] (ut/make-point position))
+ (getRank [this] rank)
+ (getMoves [this] (rank->moves this)) ;returns a list of points [x y]
  Object
  (toString [this] 
-   (println "ChessItem (" rank ") at position:" (getListPosition this) " ->" (getGridPosition this))) )
+   (println "ChessItem (" rank ") at position:" (core/getListPosition this) " ->" (core/getGridPosition this))) )
 
  (defrecord ChessMove [^ChessPiece p
                        ^clojure.lang.PersistentVector start-pos 
                        ^clojure.lang.PersistentVector end-pos]
- MoveCommand
- (try-move [this] (move-chessItem p (getEndPos this)))
- (execute [this]  (reset! (get (details) :board-atom) (try-move this))) ;STATE CHANGE!
- (undo    [this]  (move-chessItem p (getStartPos this)))
+ core/MoveCommand
+ (try-move [this] (move-chessItem p (core/getEndPos this)))
+ (execute [this]  (reset! (get (details) :board-atom) (core/try-move this))) ;STATE CHANGE!
+ (undo    [this]  (move-chessItem p (core/getStartPos this)))
  (getMovingPiece [_] p)
- (getStartPos [_] (utvector-of-doubles start-pos))
- (getEndPos   [_] (utvector-of-doubles end-pos))
+ (getStartPos [_] (ut/vector-of-doubles start-pos))
+ (getEndPos   [_] (ut/vector-of-doubles end-pos))
  Object
  (toString [this] 
-   (println "Checkers-move originating from" (getStartPos this) "to" (getEndPos this))))                
+   (println "Checkers-move originating from" (core/getStartPos this) "to" (core/getEndPos this))))                
 ;---------------------------------------------------------------------------------------------               
 (def chess-1d (range 64)) ;;the chess board as a list 
 (def ^:dynamic black-direction -1)
