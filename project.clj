23930908326e8f13558611c2ec4bb2b5909adc6e
Dimitrios Piliouras <jimpil1985@gmail.com>
2012-08-07 15:21:33 +0100

    updated core.logic version to 0.7.5

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/core.logic "0.7.4"]
+                 [org.clojure/core.logic "0.7.5"]
                  [midje "1.4.0"]
                  ;[clojure-encog "0.4.0-SNAPSHOT"]
                  ]
