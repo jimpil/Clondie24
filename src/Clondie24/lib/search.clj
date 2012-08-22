760a0b4e7e9a145c45f8495289819141a59b957c
Dimitrios Piliouras <jimpil1985@gmail.com>
2012-08-22 15:04:50 +0100

    new searching namespace that uses reducers

--- src/Clondie24/lib/search.clj
+++ src/Clondie24/lib/search.clj
@@ -1,55 +1,66 @@
 (ns Clondie24.lib.search 
-      (:require [Clondie24.lib.core :as core]))
+      (:require [Clondie24.lib.core :as core]
+                [clojure.core.reducers :as r]))
       
 (def curr-game "Before any searching we need a game-map." (promise))
+(declare score-by-count next-level my-max)
 
-(defn game-tree
-  "Generate the entire game tree lazily."
-  [root? dir board successors-fn depth]
-  {:node board
-   :direction dir
-   :children   (if (zero? depth) '()
-  ((if root? pmap map) #(game-tree false (- dir) % successors-fn (dec depth)) 
-                        (successors-fn board dir)))})
+(defrecord Tree [root direction children])
+(defrecord Move->Tree [move tree])
+(defrecord Move->Board [move board])
+(defrecord Move-Value  [move value])
+
+(defn best 
+([] Integer/MIN_VALUE)
+([best next] 
+  (if (pos? (compare (:value best) (:value next))) 
+    best next)))  ;(:move best) (:move rest)
+	                  
+(defn my-max 
+([x y] (max x y))
+([]  Integer/MIN_VALUE))
+   
+(defn my-min
+([x y] (min y x))
+([]  Integer/MAX_VALUE))             
+
+ 
+(defn game-tree "Generate the game tree."
+  [dir board successors-fn] 
+  (Tree. board dir 
+     (r/map #(Move->Tree. (:move %) 
+                          (game-tree (- dir) (:board %) successors-fn)) 
+             (successors-fn board dir))))
 
     
-(defn search [eval-fn tree]
-(letfn [(minimize [tree]  (if (seq (:children tree))
-                                (apply min
-                                  (map #(maximize %) (:children tree)))
-                           (eval-fn (:node tree) (:dir tree))))
-
-        (maximize [tree] (if (seq (:children tree))
-                                 (apply max
-                                  (map #(minimize %) (:children tree)))
-                            (eval-fn (:node tree) (:dir tree))))] 
-(trampoline minimize tree)))    
+(defn search "The recursion of min-max algorithm." 
+[eval-fn tree depth]
+(letfn [(minimize [tree d] (if (zero? d) (eval-fn (:root tree) (:direction tree))
+                            (r/reduce my-min      ;(apply min
+                                  (r/map #(maximize (:tree %) (dec d)) (:children tree)))))
+        (maximize [tree d] (if(zero? d) (eval-fn (:root tree) (:direction tree))
+                            (r/reduce my-max   ; (apply max
+                                   (r/map #(minimize (:tree %) (dec d)) (:children tree)))))] 
+(minimize tree depth)))    
     
 
 (defn evaluator
-  "Dynamic evaluation of a game tree. Returns a number representing how good
-  the root position is. Uses minimax algorithm"
-  [eval-fn]
+  [eval-fn depth]
   (fn [t]
-    (search eval-fn t)))
-
-(defn best-next-state
-  "Get the best next state for the given game tree.
-  static-evaluator evaluates single positions, without looking at the tree, and
-  returning a number."
-  [tree static-evaluator]
-  (->> (:children tree)
-       (apply max-key (evaluator static-evaluator))
-       (:node)))
-              
+    (search eval-fn t depth)))
+            
                 
 (defn next-level [b dir] 
-(let [team (filter #(= dir (:direction %)) b) ;all the team-mates (with same direction)
-      team-moves (concat (for [piece team 
-                               coords (core/getMoves piece)] 
-                           (core/dest->Move @curr-game piece coords)))]
- (vec (map core/try-move team-moves))))
+  (r/map #(Move->Board. % (core/try-move %)) 
+    (core/team-moves @curr-game b dir)))
 
+(defn fake [dir b d]
+(r/fold 1 best best
+ (r/map #(Move-Value. (:move %) (search score-by-count (:tree %) d))
+                       (into [] (:children (game-tree dir b next-level))))))
+                         
+#_(defn fake2 [dir b d] 
+(r/fold best  (into [] (:children (game-tree dir b next-level)))))
 
 (defn score-by-count [b dir] 
 (let [hm (filter #(= dir (:direction %)) b)
@@ -58,24 +69,11 @@
     (count (remove nil? aw)))))      
  
  
- (defn score-naive [b dir] 
+ (defn score-naive [b dir]
  (let [hm (filter #(= dir (:direction %)) b)
        aw (remove nil? (filter #(not= dir (:direction %)) b))]
  (- (apply + (map #(:value %) hm)) 
-    (apply + (map #(:value %) aw)))))
-    
-(defn start-search [dir root-n depth]
-(let [b-state (-> (game-tree true dir root-n next-level depth)
-                  (best-next-state score-by-count ))
-      team    (filter #(= dir (:direction %)) root-n)
-      team-moves (concat (for [piece team 
-                               coords (core/getMoves piece)] 
-                         (core/dest->Move @curr-game piece coords)))]
-(some #(when (= b-state (core/try-move %)) %) team-moves))) 
-;the best move is the one that results to the best next state as returned by the recursion   
+    (apply + (map #(:value %) aw)))))   
 
-;(reset! search/curr-game details) 
-    
-  ;(-> (game-tree true 1 b next-level 8) 
-  ;    (best-next-state score-naive))
+
                 
