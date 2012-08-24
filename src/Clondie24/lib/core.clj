9a2fdf32b666e7c17384eed260170d572e884143
Dimitrios Piliouras <jimpil1985@gmail.com>
2012-08-24 18:42:11 +0100

    optimisations

--- src/Clondie24/lib/core.clj
+++ src/Clondie24/lib/core.clj
@@ -1,5 +1,6 @@
 (ns Clondie24.lib.core
-   (:require [Clondie24.lib.util :as ut]))
+   (:require [Clondie24.lib.util :as ut]
+             [clojure.core.reducers :as r :only [map filter]]))
 ;----------------------------------------<SOURCE>--------------------------------------------------------------------
 ;----------------------------------------<CODE>----------------------------------------------------------------------   
 
@@ -20,30 +21,31 @@
  (getPoint [this])
  (die [this])
  (promote [this])
- (getMoves [this])) 
+ (getMoves [this b])) 
  
  (defprotocol Movable 
  "The Command design pattern in action (allows us to do/undo moves)."
  (try-move [this]) 
  (getOrigin [this]))
  
-(defn translate-position
+(defn translate
 "Translates a position from 1d to 2d and vice-versa. 
 Mappings should be either 'checkers-board-mappings' or 'chess-board-mappings'." 
-([i mappings] ;{:post [(not (nil? %))]}   
+([^long i mappings] ;{:post [(not (nil? %))]}   
   (let [grid-loc (get mappings i)] ;will translate from 1d to 2d
     (if-not (nil? grid-loc) grid-loc ;not found 
     (throw (IllegalStateException. (str "NOT a valid list-location:" i))))))
 ([x y ^clojure.lang.PersistentVector mappings] ;{:post [(not (== % -1))]} 
-  (let [list-loc (.indexOf mappings (vector x y))] ;will translate from 2d to 1d
+  (let [list-loc (.indexOf mappings [x y])] ;will translate from 2d to 1d
     (if-not (= list-loc -1) list-loc ;not found
-    (throw (IllegalStateException. (str "NOT a valid grid-location: [" x ", " y "]")))))))
+     (throw (IllegalStateException. (str "NOT a valid grid-location: [" x ", " y "]")))))))
 
+(def translate-position (memoize translate))
                     
 (defn make-piece 
-"The central function for creating pieces. A piece is simply a record with 4 keys."
- [game c pos &{:keys [rank direction r-value]
-               :or {rank 'zombie direction 1 r-value 1}}]
+"Helper function for creating pieces. A piece is simply a record with 4 keys."
+ [game c pos rank direction] ;&{:keys [rank direction ]
+               ;:or {rank 'zombie direction 1 r-value 1}}]
  ((ut/record-factory-aux (:record-name game)) c pos rank 
   ((keyword rank) (:rel-values game)) direction  
   {:alive true}   ;pieces are born 'alive'             
@@ -59,33 +61,35 @@ Mappings should be either 'checkers-board-mappings' or 'chess-board-mappings'."
      (conj p2 (conj (repeat vacant nil) p1))))))
   
  
-(defn alive? [p]
-(:alive (meta p)))
+(definline alive? [p]
+`(:alive (meta ~p)))
 
 (defn empty-board 
 "Returns an empty board for the game provided - all nils." 
 [game] 
 (vec (repeat (:board-size game) nil)))
 
-(defn populate-board 
+(definline populate-board 
 "Builds a new board with nils where dead pieces." 
 [board]     
-(into [] (map #(if (alive? %) % nil) board)))
+`(into [] (r/map #(if (alive? %) % nil) ~board)))
                
 
 (defn move 
 "The function responsible for moving Pieces. Each piece knows how to move itself. Returns the resulting board without making any state changes. " 
-[game-map p coords] 
+[board p coords] 
 ;{:pre [(satisfies? Piece p)]}  ;safety comes first
-(if  (some #{coords} (:mappings game-map)) ;check that the position exists on the grid
+;(if  (some #{coords} (:mappings game-map)) ;check that the position exists on the grid
 (let [newPiece (update-position p coords) ;the new piece as a result of moving 
       old-pos  (getListPosition p)
       new-pos  (getListPosition newPiece)]            
-(-> @(:board-atom game-map)  ;deref the appropriate board atom 
-     (assoc old-pos nil) 
-     (assoc new-pos newPiece)
-     (populate-board))) ;replace dead-pieces with nils
-(throw (IllegalStateException. (str coords " is NOT a valid position according to the mappings provided!"))))) 
+(->  board 
+     (transient)  
+     (assoc! old-pos nil) 
+     (assoc! new-pos newPiece)
+     (persistent!)
+     #_(populate-board))) ;replace dead-pieces with nils
+#_(throw (IllegalStateException. (str coords " is NOT a valid position according to the mappings provided!")))) 
 
 (defrecord Move [p mover ^clojure.lang.PersistentVector end-pos]
  Movable
@@ -95,15 +99,15 @@ Mappings should be either 'checkers-board-mappings' or 'chess-board-mappings'."
  (toString [this] 
    (println "Move from:" (getOrigin this) "to:" end-pos)))   
 
-(defn dest->Move "Helper fn for creating moves." 
-[dm p dest]  (Move. p (partial move dm) dest))
+(definline dest->Move "Constructor for creating moves." 
+[b p dest]  `(Move. ~p #(move ~b %1 %2) ~dest))
 
 (defn execute! [^Move m batom]
  (reset! batom (try-move m)))
  
-(defn threatens? "Returns true if 2 is threatened by p1." 
-[p2 p1]
-(if (some #{(:position p2)} (getMoves p1)) true false)) 
+(definline threatens? "Returns true if 2 is threatened by p1 on board b." 
+[p2 p1 b]
+`(if (some #{(:position ~p2 ~b)} (getMoves ~p1 ~b)) true false)) 
 
 (defn undo! []
 (swap! board-history (comp vec butlast)))     
@@ -113,17 +117,17 @@ Mappings should be either 'checkers-board-mappings' or 'chess-board-mappings'."
 ;(format "%c%d" letter number)))
 
 (defn clear-history! []
- (swap! board-history empty))
+ (swap! board-history empty)) 
 
-(defn gather-team "Returns all the pieces with same direction dir on this board b." 
+(definline gather-team "Returns all the pieces with same direction dir on this board b." 
 [b dir]
-(filter #(= dir (:direction %)) b)) ;all the team-mates (with same direction)
+`(r/filter #(= ~dir (:direction %)) ~b)) ;all the team-mates (with same direction)
  
-(defn team-moves 
-[game b dir]
-(let [team (gather-team b dir) 
-      tmvs (mapcat (fn [p] (map #(dest->Move game p %) (getMoves p))) team)]
- tmvs)) 
+(definline team-moves 
+[b dir]
+`(let [team# (gather-team ~b ~dir) 
+       tmvs# (r/mapcat (fn [p#] (r/map #(dest->Move ~b p# %) (getMoves p# ~b))) team#)]
+ tmvs# ))
 
 ;EXAMPLEs:
  ;(make-checker    (make-color 'BLUE)  [1 5] :rank 'soldier)
@@ -135,10 +139,10 @@ Mappings should be either 'checkers-board-mappings' or 'chess-board-mappings'."
   [m b pos]
  (let [[x y] pos]
  (nil? 
-  (nth b (translate-position x y m)))))  
+  (get b (translate-position x y m)))))  
  
-(defn bury-dead [c]
- (filter alive? c))  
+(definline bury-dead [c]
+ `(filter alive? ~c))  
 
 (defn collides? "Returns true if the move from [sx sy] to [ex ey] collides with any friendly pieces. The move will be walked step by step by the walker fn."
 [[sx sy] [ex ey] walker b m dir]
@@ -153,9 +157,9 @@ Mappings should be either 'checkers-board-mappings' or 'chess-board-mappings'."
 (let [next-b (try-move move)
      def-king (filter #(and (= 'king (:rank %)) 
                             (= (:direction %) (:direction (:p move)))) next-b) ;the king we're defending
-     opp-dir (- (:direction (:p move))) ;negate direction of moving piece to get opponent
+     opp-dir (unchecked-negate (:direction (:p move))) ;negate direction of moving piece to get opponent
      opp-pieces (gather-team b opp-dir)]
-(every? (partial threatens? def-king) opp-pieces)))
+(every? #(threatens? def-king % b) opp-pieces)))
                  
 
   
