73a6f6cc95498cc4e7cbbd84374750b63b1fc1fa
Dimitrios Piliouras <jimpil1985@gmail.com>
2012-09-19 20:43:23 +0100

    updated enclog dependency

--- project.clj
+++ project.clj
@@ -5,10 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]
                  ;[org.clojure/clojure "1.5.0-alpha3"]
-                 [org.clojure/core.logic "0.7.5"]
-                ; [midje "1.4.0"] 
-                 [seesaw "1.4.2" :exclusions [org.clojure/clojure]]
-                 ;[clojure-encog "0.4.1-SNAPSHOT"] ;not needed yet
+                 [org.clojure/core.logic "0.8-alpha2" ] ; 
+                ; [midje "1.4.0"]
+                 [ghostandthemachine/seesaw "1.4.3-SNAPSHOT" :exclusions [org.clojure/clojure]] 
+                 ;[seesaw "1.4.2" :exclusions [org.clojure/clojure]]
+                 [enclog "0.5.7-SNAPSHOT"] 
                  ]
   :dev-dependencies [[midje "1.4.0"]]               
   ;:plugins [[lein-swank "1.4.4"]]             
@@ -16,5 +17,5 @@
   :warn-on-reflection true
  ;:javac-options {:classpath "target/dependency/encog-core-3.1.0.jar" :destdir "target/classes"}
  ;:java-source-path "src/java"
- :main     Clondie24.games.chess             
+ ;:main     Clondie24.games.checkers             
  )
