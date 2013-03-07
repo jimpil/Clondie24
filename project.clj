9a3bca0598c8c167a0c4e60111fb6156390a0575
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-03-07 17:27:42 +0000

    bumped versions

--- project.clj
+++ project.clj
@@ -3,17 +3,17 @@
   :url "https://github.com/jimpil/Clondie24"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.trace "0.7.3"]
-                 [org.clojure/core.logic "0.8.0-beta5" ] ; 
+                 [org.clojure/core.logic "0.8.0-rc2" ] ; 
                  ;[midje "1.5-alpha2"]
-                 [org.clojure/core.match "0.2.0-alpha11"]
+                 [org.clojure/core.match "0.2.0-alpha12"]
                  [ghostandthemachine/seesaw "1.4.3-SNAPSHOT" :exclusions [org.clojure/clojure]] 
                  [enclog "0.5.8"] 
                  ]
   :dev-dependencies [[[midje "1.5-alpha2"]]]               
   ;:plugins [[lein-swank "1.4.4"]]             
-  :jvm-opts ["-Xmx2g" "-server"] 
+  :jvm-opts ["-Xmx2g" "-server" "-XX:-UseCompressedOops"] 
  ;:javac-options {:classpath "target/dependency/encog-core-3.1.0.jar" :destdir "target/classes"}
  ;:java-source-path "src/java"
  :main     Clondie24.games.chess             
