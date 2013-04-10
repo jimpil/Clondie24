6efaa1663ee2b90be4ff33b4d04faa3f6b7dae7c
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-04-11 00:24:03 +0100

    new clojrue version

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
   :url "https://github.com/jimpil/Clondie24"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.trace "0.7.3"]
                  [org.clojure/core.logic "0.8.0-rc2" ] ; 
-                 ;[midje "1.5-alpha2"]
                  [org.clojure/core.match "0.2.0-alpha12"]
-                 [ghostandthemachine/seesaw "1.4.3-SNAPSHOT" :exclusions [org.clojure/clojure]] 
-                 [enclog "0.5.8"] 
+                 [org.clojure/tools.nrepl "0.2.2"] 
+                 [seesaw "1.4.3"]
+                 [enclog "0.5.8"]
+                 [swingrepl "1.3.0" :exclusions [org.clojure/clojure]] 
                  ]
   :dev-dependencies [[[midje "1.5-alpha2"]]]               
   ;:plugins [[lein-swank "1.4.4"]]             
