d583c24deab1f24aaec8024a6db7961dda11cca3
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-09-16 00:08:01 +0100

    slight refactoring

--- src/Clondie24/lib/search.clj
+++ src/Clondie24/lib/search.clj
@@ -56,15 +56,30 @@
         (maximize  [tree d] (if (zero? d) (eval-fn (:root tree) (:direction tree))
                             (r/reduce my-max   
                                    (r/map (fn [child] (minimize (:tree child) (dec d))) (:children tree)))))] 
-(minimize tree (-> (:pref-depth @curr-game) dec int))))
+(minimize tree (-> (:pref-depth @curr-game) dec))))
 
 ;(def minmax-TT (memoize #(minmax (:scorer @curr-game) %)));;transposition tables
 ;(def minmax-TT (memoize minmax))
 
 
-
 (defn alpha-beta "The recursion of the min-max algorithm with pruning." 
-[eval-fn tree] 
+[eval-fn tree a b] 
+(letfn [(minimize  [tree d] 
+         (if (zero? d) (eval-fn (:root tree) (:direction tree))
+          ;(reset! a 
+            (r/reduce my-min 
+              (r/map (fn [child] (let [ret (maximize (:tree child) (dec d))] 
+                                  (if (<= a ret) (reduced a) ret))) (:children tree)))))
+        (maximize  [tree d] 
+        (if (zero? d) (eval-fn (:root tree) (:direction tree))
+        ; (reset! b 
+           (r/reduce my-max 
+            (r/map (fn [child] (let [ret (minimize (:tree child) (dec d))] 
+                                (if (>= b ret) (reduced b) ret))) (:children tree)))))]             
+(minimize tree (-> (:pref-depth @curr-game) dec))) )
+
+#_(defn alpha-beta "The recursion of the min-max algorithm with pruning." 
+[eval-fn tree ] 
 (letfn [(minimize  [tree d a b] (if (zero? d) (let [res (eval-fn (:root tree) (:direction tree))]
                                                 (do (swap! b my-max res) res))
 (let [ret (r/reduce (fn ([x y] (if (< @a @b) (do #_(println {:Depth d :A @a :B @b}) (reduced @a)) (my-min x y))) 
@@ -80,12 +95,17 @@
 (minimize tree (dec (:pref-depth @curr-game)) (atom 1000) (atom -1000))))
 
 (def alpha-beta-TT (memoize alpha-beta));;transposition tables
+               
+(defn search* 
+([eval-fn tree a b]
+ (if (and (nil? a) 
+          (nil? b))
+  (search* eval-fn tree)
+  (alpha-beta eval-fn tree a b)))
+([eval-fn tree]
+  (minmax eval-fn tree)) )               
 
-(defn search* [eval-fn tree pruning?]
-  (if pruning? (alpha-beta eval-fn tree)
-               (minmax eval-fn tree)))
-
-(def search-mem (memoize search*))
+(def search-mem (memoize search*)) ;;???
     
 
 #_(defn evaluator
@@ -98,12 +118,14 @@
  (r/map #(Move->Board. % (core/try-move %)) 
    ((:team-moves @curr-game) b dir false))) ;performance cheating again!
 
-(defn go [^long dir b pruning?]
-(let [successors (into [] (:children (game-tree dir b next-level)))]
+(defn go [player b pruning?]
+(let [successors (into [] (:children (game-tree (:direction player) b next-level)))
+      a (if pruning? (search* (get player :brain (:naive-scorer @curr-game)) (:tree (first successors))) nil)
+      b (if (nil? a) nil (- a))]
   (if (= 1 (count successors)) (first successors)   ;;mandatory move detected - just return it  
 (r/fold (:chunking @curr-game) best best ;2 = best so far
- (r/map #(Move-Value. (:move %) (search-mem (:scorer @curr-game) (:tree %) pruning?)) ;starting from children so decrement depth
-                    successors )))))
+ (r/map #(Move-Value. (:move %) (search* (get player :brain (:naive-scorer @curr-game)) (:tree %) a b)) ;starting from children so decrement depth
+                    (if pruning? (rest successors) successors) )))))
 ;;---------------------------------------------------------------------------------------------------------                         
 ;--------------------------------LAZY-VERSION-------------------------------------------------------------- 
 
@@ -163,16 +185,16 @@
 (minimize tree-parent)))
            
                 
-(defn next-level-lazy [b ^long dir] 
+(defn next-level-lazy [b dir] 
  (map #(Move->Board. % (core/try-move %)) 
    ((:team-moves @curr-game) b dir nil)))
 
-(defn go-lazy [^long dir b]
-(let [successors  (:children (game-tree-lazy dir b next-level-lazy))]
+(defn go-lazy [player b]
+(let [successors  (:children (game-tree-lazy (:direction player) b next-level-lazy))]
   (case  (count successors)
       1  (first successors)   ;;mandatory move detected - just return it  
       0 nil
 (apply max-key :value 
- (map #(Move-Value. (:move %) (minmax-ttt-lazy (:scorer @curr-game) (:tree %))) 
+ (map #(Move-Value. (:move %) (minmax-ttt-lazy (get player :brain (:naive-scorer @curr-game)) (:tree %))) 
                     successors )))))
 
